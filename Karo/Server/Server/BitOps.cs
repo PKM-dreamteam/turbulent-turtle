b923854a367d4ee8bc756446c42e89d5e3958b95
Robert Miroszewski <r.miroszewski@gmail.com>
2015-11-23 18:14:26 +0100

    Wersja protokołu przesyłania v1.1
    -Zmieniono kolejność bajtów na zgodną ze standardem sieciowym (być może
    to był problem)
    -Powrócono do zmiennych typu Integer dla wartości x, y, z i v
    -Dodano przykładowy pakiet wraz z rozpisaniem na poszczególne bajty

---
+++ Karo/Server/Server/BitOps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class BitOps
+    {
+        public static string ConvertToHexString(byte input)
+        {
+            try
+            {
+                return input.ToString("X") + "h";
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return "0x0";
+        }
+
+        public static void Set(ref byte aByte, int pos, bool value)
+        {
+            if (value)
+            {
+                //left-shift 1, then bitwise OR
+                aByte = (byte)(aByte | (1 << pos));
+            }
+            else
+            {
+                //left-shift 1, then take complement, then bitwise AND
+                aByte = (byte)(aByte & ~(1 << pos));
+            }
+        }
+
+        public static bool Get(byte aByte, int pos)
+        {
+            //left-shift 1, then bitwise AND, then check for non-zero
+            return ((aByte & (1 << pos)) != 0);
+        }
+    }
+}
--- Karo/Server/Server/DataPack.cs
+++ Karo/Server/Server/DataPack.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,17 @@ namespace Server
         public byte deviceId;
         public long timestamp;
 
-        public double x;
-        public double y;
-        public double z;
-        public double velocity;
+        public int x;
+        public int y;
+        public int z;
+        public int velocity;
 
         public byte[] data;
 
 
-        public DataPack(byte _deviceId, long _timestamp, double _x, double _y, double _z, double _velocity, byte[] _data = null)
+        public DataPack(byte _deviceId, long _timestamp, int _x, int _y, int _z, int _velocity, byte[] _data = null)
         {
-            length = 1 + 1 + 8 + 4 * 8;
+            length = 1 + 1 + 8 + 4 * 4;
             deviceId = _deviceId;
             timestamp = _timestamp;
             x = _x;
@@ -41,27 +42,78 @@ namespace Server
             }
         }
 
-        public static bool GetBit(byte b, int bitNumber)
-        {
-            return (b & (1 << bitNumber - 1)) != 0;
-        }
-
-        public DataPack(byte[] _data)
+        public static void PrintBits(byte [] input, string title = "")
         {
             string dataStr = "";
-
-            foreach (byte one in _data)
+            if (BitConverter.IsLittleEndian)
             {
+                for(int j = input.Length - 1; j >= 0; j--)
+                {
+                    for (int i = 7; i >= 0; i--)
+                    {
+                        if (BitOps.Get(input[j], i))
+                            dataStr += "1";
+                        else
+                            dataStr += "0";
+                    }
+                    dataStr += "\n";
+                }
+            }
+            else
+            {
+                for (int j = 0; j < input.Length; j++)
+                {
+                    for (int i = 0; i < 8; i++)
+                    {
+                        if (BitOps.Get(input[j], i))
+                            dataStr += "1";
+                        else
+                            dataStr += "0";
+                    }
+                    dataStr += "\n";
+                }
+            }
+
+            if (title != "")
+                Console.WriteLine(title + ":\n");
+            else
+                Console.WriteLine("Data:\n");
+
+            Console.WriteLine(dataStr);
+            Console.WriteLine("--------------------------------------------------------------------");
+        }
+    
+        public static void PrintHex(byte[] input, string title = "")
+        {
+            string dataStr = "";
+            for (int j = 0; j < input.Length; j++)
+            {
+                dataStr += "0x" + BitConverter.ToString(input, j, 1);//BitOps.ConvertToHexString(input[j]);
+
+                dataStr += "\t";
                 for (int i = 0; i < 8; i++)
                 {
-                    if (GetBit(one, i))
+                    if (BitOps.Get(input[j], i))
                         dataStr += "1";
                     else
                         dataStr += "0";
                 }
+
+                dataStr += "\n";
             }
 
-            Console.WriteLine("Data: " + dataStr);
+            if (title != "")
+                Console.WriteLine(title + ":\n");
+            else
+                Console.WriteLine("Data:\n");
+
+            Console.WriteLine(dataStr);
+            Console.WriteLine("--------------------------------------------------------------------");
+        }
+
+        public DataPack(byte[] _data)
+        {
+            PrintBits(_data);
 
             length = _data[0];
             deviceId = _data[1];
@@ -75,18 +127,18 @@ namespace Server
             
             Array.Copy(_data, 2, dataTimeBytes, 0, 8);
             Array.Copy(_data, 10, xBytes, 0, 4);
-            Array.Copy(_data, 18, yBytes, 0, 4);
-            Array.Copy(_data, 26, zBytes, 0, 4);
-            Array.Copy(_data, 34, velBytes, 0, 4);
-            Array.Copy(_data, 42, data, 0, data.Length);
+            Array.Copy(_data, 14, yBytes, 0, 4);
+            Array.Copy(_data, 18, zBytes, 0, 4);
+            Array.Copy(_data, 22, velBytes, 0, 4);
+            Array.Copy(_data, 26, data, 0, data.Length);
   
             
-            timestamp = BitConverter.ToInt64(dataTimeBytes, 0);
+            timestamp = IPAddress.NetworkToHostOrder(BitConverter.ToInt64(dataTimeBytes, 0));
 
-            x = BitConverter.ToDouble(xBytes, 0);
-            y = BitConverter.ToDouble(yBytes, 0);
-            z = BitConverter.ToDouble(zBytes, 0);
-            velocity = BitConverter.ToDouble(velBytes, 0);
+            x = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(xBytes, 0));
+            y = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(yBytes, 0));
+            z = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(zBytes, 0));
+            velocity = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(velBytes, 0));
             
 
         }
@@ -97,22 +149,32 @@ namespace Server
 
             byte[] lengthBytes = BitConverter.GetBytes(length);
             byte[] deviceID = BitConverter.GetBytes(deviceId);
-            byte[] xBytes = BitConverter.GetBytes(x);
-            byte[] yBytes = BitConverter.GetBytes(y);
-            byte[] zBytes = BitConverter.GetBytes(z);
-            byte[] velBytes = BitConverter.GetBytes(velocity);
-            byte[] dataTimeBytes = BitConverter.GetBytes(timestamp);
 
+            byte[] dataTimeBytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(timestamp));
+            byte[] xBytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(x));
+            byte[] yBytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(y));
+            byte[] zBytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(z));
+            byte[] velBytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(velocity));
+
+
+            //PrintBits(lengthBytes, "lengthBytes");
+            //PrintBits(deviceID, "deviceID");
+            //PrintBits(dataTimeBytes, "dataTime");
+            //PrintBits(xBytes, "xBytes");
+            //PrintBits(yBytes, "yBytes");
+            //PrintBits(zBytes, "zBytes");
+            //PrintBits(velBytes, "velBytes");
+            
 
             dataBytes[0] = lengthBytes[0];
             dataBytes[1] = deviceID[0];
 
             Array.Copy(dataTimeBytes, 0, dataBytes, 2, dataTimeBytes.Length);
             Array.Copy(xBytes, 0, dataBytes, 10, xBytes.Length);
-            Array.Copy(yBytes, 0, dataBytes, 18, yBytes.Length);
-            Array.Copy(zBytes, 0, dataBytes, 26, zBytes.Length);
-            Array.Copy(velBytes, 0, dataBytes, 34, velBytes.Length);
-            Array.Copy(data, 0, dataBytes, 42, data.Length);
+            Array.Copy(yBytes, 0, dataBytes, 14, yBytes.Length);
+            Array.Copy(zBytes, 0, dataBytes, 18, zBytes.Length);
+            Array.Copy(velBytes, 0, dataBytes, 22, velBytes.Length);
+            Array.Copy(data, 0, dataBytes, 26, data.Length);
 
             
             return dataBytes;
